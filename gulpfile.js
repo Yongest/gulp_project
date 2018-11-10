33f5249f80a0ccda26a8173654b542207d0c30ce
yongest <1046788379@qq.com>
2018-11-10 14:46:40 +0800

    项目初始化

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var postcss = require('gulp-postcss');
+var gulp = require('gulp');
+
+
+gulp.task('css', function () {
+    return gulp.src('./src/*.css')
+        .pipe(postcss())
+        .pipe(gulp.dest('./dest'));
+});
